d7d4a1f8ca5391833a4f995e3af591a72707b01f
Mark Struberg <struberg@apache.org>
2014-08-08 20:48:09 +0200

    RequestScoped bean doesn't need to be Serializable
    It gets proxied by only injecting the Contextual Reference anyway.
    And this proxy is perfectly serializable...

--- OwbBusinessCDI/src/main/java/br/com/cdi/BusinessBean.java
+++ OwbBusinessCDI/src/main/java/br/com/cdi/BusinessBean.java
@@ -1,26 +1,20 @@
 package br.com.cdi;
 
-import java.io.Serializable;
 
 @javax.inject.Named
 @javax.enterprise.context.RequestScoped
-public class BusinessBean implements Serializable {
+public class BusinessBean {
 
-	/**
-	 *  Atributo '<code>serialVersionUID</code>' do tipo long
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	private String nome;
-	
-	/**
-	 * @return the nome
-	 */
-	public String getNome() {
-		return nome;
-	}
+    private String nome;
 
-	public void execute() {
-		System.out.println(getClass().getSimpleName() +  " executed");
-	}
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    public void execute() {
+        System.out.println(getClass().getSimpleName() +  " executed");
+    }
 }
